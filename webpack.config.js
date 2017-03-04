c2890e1c0008eb1481ac71ecaea053c02b7bfbab
frosv <616342684@qq.com>
2017-03-04 16:39:06 +0800

    基础配置完成 但是还有很多的疑问 css并没有区分

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+const config = {
+  entry: {
+    play: './src/play/index.js',
+    list: './src/list/list.js',
+    commons: ['jquery']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-[hash:8].js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: 'babel-loader'
+    }, {
+      test: /\.css$/,
+      use: ExtractTextWebpackPlugin.extract({
+        fallback: "style-loader",
+        use: "css-loader"
+      })
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      use: 'url-loader'
+    }, {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      use: 'url-loader'
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      use: 'url-loader'
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      use: 'url-loader'
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      use: 'file-loader'
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      use: 'url-loader'
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename:'play.html',
+      template: './src/play/play.html',
+      chunks: ['play', 'commons'],
+      inject: 'body'
+    }),
+    new HtmlWebpackPlugin({
+      filename:'list.html',
+      template: './src/list/list.html',
+      chunks: ['list', 'commons'],
+      inject: 'body'
+    }),
+    new ExtractTextWebpackPlugin('styles.css'),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      _: 'lodash',
+      lodash: 'lodash'
+    })
+  ]
+}
+
+module.exports = config;
