e3950e83644c0d448434c5946d466c0c9fd3a247
frosv <616342684@qq.com>
2017-03-07 18:10:42 +0800

    更新文件名

--- src/play/index.js
+++ src/play/index.js
@@ -1,7 +1,7 @@
 // 引入各种组件，编写起来方便也能很好的做到相互修改不影响
 
 //css文件引入
-require('./css/index.css');
+require('./css/play.css');
 require('../../node_modules/bootstrap/dist/css/bootstrap.css');
 
 //js文件引入
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 //引入需要的模块
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//多条目css
+
+
+
+// const extractPlay = new　 ExtractTextWebpackPlugin　('./src/play/css/play.css');
+// const extractList = new　 ExtractTextWebpackPlugin　('./src/list/css/list.css');
+
 
 
 
@@ -10,36 +18,36 @@ const config = {
   devtool: 'eval',
   //入口文件
   entry: {
-    play: './src/play/index.js',//单个文件入口
+    play: './src/play/index.js', //单个文件入口
     list: './src/list/list.js',
-    commons: ['jquery']//公共文件
+    commons: ['jquery'] //公共文件
   },
   //编译后输出文件
   output: {
-    path: path.resolve(__dirname, 'dist'),//输出文件夹
-    filename: '[name]-[hash:8].js'//输出文件名
+    path: path.resolve(__dirname, 'dist'), //输出文件夹
+    filename: '[name]-[hash:8].js' //输出文件名
   },
   //本地服务器配置
   devServer: {
-    contentBase: path.join(__dirname, "dist"),//基础引用地址
-    compress: true,//使用gzip压缩
-    port: 8080,//端口号
-    host:"0.0.0.0",//允许外部访问（然而我为什么访问不了。）
+    contentBase: path.join(__dirname, "dist"), //基础引用地址
+    compress: true, //使用gzip压缩
+    port: 8080, //端口号
+    host: "0.0.0.0", //允许外部访问（然而我为什么访问不了。）
     // contentBase: [path.join(__dirname, "public"), path.join(__dirname, "assets")],
     //多个文件夹配置方式
     // headers:{"FrosV":"FUOK♂YOU"}
-    hot: true//热处理
+    hot: true //热处理
   },
   //模块化
   module: {
-    rules: [{//读取规则
-      test: /\.(js|jsx)$/,//匹配js
-      use: 'babel-loader'//使用babel编译
+    rules: [{ //读取规则
+      test: /\.(js|jsx)$/, //匹配js
+      use: 'babel-loader' //使用babel编译
     }, {
-      test: /\.css$/,//匹配css
-      use: ExtractTextWebpackPlugin.extract({
-        fallback: "style-loader",
-        use: "css-loader"
+      test: /\.css$/, //匹配css
+      // use: 'css-loader'
+      use: ExtractTextPlugin.extract({
+        use: 'css-loader'
       })
     }, {
       test: /\.(png|jpg|gif)$/,
@@ -63,11 +71,11 @@ const config = {
   },
   //插件
   plugins: [
-    new HtmlWebpackPlugin({//html注入文件插件
-      filename: 'play.html',//文件名
-      template: './src/play/play.html',//渲染模板
-      chunks: ['play', 'commons'],//原型
-      inject: 'body'//？？？
+    new HtmlWebpackPlugin({ //html注入文件插件
+      filename: 'play.html', //文件名
+      template: './src/play/play.html', //渲染模板
+      chunks: ['play', 'commons'], //原型
+      inject: 'body' //？？？
     }),
     new HtmlWebpackPlugin({
       filename: 'list.html',
@@ -75,13 +83,15 @@ const config = {
       chunks: ['list', 'commons'],
       inject: 'body'
     }),
-    new ExtractTextWebpackPlugin('styles.css'),//css渲染模板
-    new webpack.ProvidePlugin({//？？？
+    new webpack.ProvidePlugin({ //？？？
       $: 'jquery',
       jQuery: 'jquery',
       _: 'lodash',
       lodash: 'lodash'
-    })
+    }),
+    new ExtractTextPlugin('styles.css')
+    // extractPlay,
+    // extractList
   ]
 }
 
